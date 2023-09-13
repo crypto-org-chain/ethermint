31c0e5eb5cb0f4310d1f95d9d15a81dfcfb5b447
HuangYi <huang@crypto.com>
2023-09-13 10:37:41 +0800

    cleanup

--- rpc/namespaces/ethereum/eth/filters/api.go
+++ rpc/namespaces/ethereum/eth/filters/api.go
@@ -55,7 +55,6 @@ type Backend interface {
 	HeaderByNumber(blockNum types.BlockNumber) (*ethtypes.Header, error)
 	HeaderByHash(blockHash common.Hash) (*ethtypes.Header, error)
 	TendermintBlockByHash(hash common.Hash) (*coretypes.ResultBlock, error)
-	TendermintBlockByNumber(types.BlockNumber) (*coretypes.ResultBlock, error)
 	TendermintBlockResultByNumber(height *int64) (*coretypes.ResultBlockResults, error)
 	GetLogs(blockHash common.Hash) ([][]*ethtypes.Log, error)
 	GetLogsByHeight(*int64) ([][]*ethtypes.Log, error)
