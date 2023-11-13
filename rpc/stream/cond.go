4c035d8bf7cd9de37ed9ee59540d23153cc62880
HuangYi <huang@crypto.com>
2023-11-13 15:05:28 +0800

    fix cond race

--- rpc/stream/cond.go
+++ rpc/stream/cond.go
@@ -1,20 +1,28 @@
 package stream
 
-import "context"
+import (
+	"context"
+	"sync"
+)
 
 // Cond implements conditional variable with a channel
 type Cond struct {
+	mu sync.Mutex // guards ch
 	ch chan struct{}
 }
 
 func NewCond() *Cond {
-	return &Cond{make(chan struct{})}
+	return &Cond{ch: make(chan struct{})}
 }
 
 // Wait returns true if the condition is signaled, false if the context is canceled
 func (c *Cond) Wait(ctx context.Context) bool {
+	c.mu.Lock()
+	ch := c.ch
+	c.mu.Unlock()
+
 	select {
-	case <-c.ch:
+	case <-ch:
 		return true
 	case <-ctx.Done():
 		return false
@@ -22,6 +30,8 @@ func (c *Cond) Wait(ctx context.Context) bool {
 }
 
 func (c *Cond) Broadcast() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	close(c.ch)
 	c.ch = make(chan struct{})
 }
