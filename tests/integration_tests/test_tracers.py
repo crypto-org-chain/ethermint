3d99b094766b714a30f456694346722d86b0415c
Xinyu Zhao <xinyu.zhao@crypto.com>
2023-11-17 14:11:57 +0800

    tests: add bigramTracers integration tests

--- tests/integration_tests/test_tracers.py
+++ tests/integration_tests/test_tracers.py
@@ -64,7 +64,7 @@ def test_tracers(ethermint_rpc_ws):
     )
     tx_hash = tx["transactionHash"].hex()
 
-    w3_wait_for_new_blocks(w3, 1)
+    w3_wait_for_new_blocks(w3, 1, sleep=0.1)
 
     tx_res = eth_rpc.make_request(
         "debug_traceTransaction", [tx_hash, {"tracer": "callTracer"}]
@@ -97,84 +97,71 @@ def test_debug_tracecall_insufficient_funds(ethermint_rpc_ws):
     assert "error" in tx_res
     assert tx_res["error"] == {"code": -32000, "message": "rpc error: code = Internal desc = insufficient balance for transfer"}, ""
 
-    # # Default from address is zero address
-    # oneCro = 1000000000000000000
-    # tx = {"to": "0x0000000000000000000000000000000000000000", "value": oneCro, "gasPrice": gas_price}
-    # send_transaction(w3, tx)
-    # tx = {
-    #     "to": ADDRS["community"],
-    #     "value": hex(100),
-    #     "gasPrice": hex(gas_price),
-    #     "gas": hex(21000),
-    # }
-    # balance_res = eth_rpc.make_request("eth_getBalance", [
-    #     "0x0000000000000000000000000000000000000000", "latest"
-    # ])
-    # print(balance_res)
-    # tx_res = eth_rpc.make_request("debug_traceCall", [tx, "latest", {
-    #     "tracer": "prestateTracer"
-    # }])
-    # print(tx_res)
-    # assert tx_res["result"] == EXPECTED_STRUCT_TRACER, ""
+    from_addr = ADDRS["validator"]
+    to_addr = ADDRS["community"]
+    tx = {
+        "from": from_addr,
+        "to": to_addr,
+        "value": hex(100),
+        "gas": hex(21000),
+    }
 
-    # tx_res = eth_rpc.make_request("debug_traceCall", [tx, "latest"])
-    # print(tx_res)
-    # assert tx_res["result"] == EXPECTED_STRUCT_TRACER, ""
+    tx_res = eth_rpc.make_request("debug_traceCall", [tx, "latest"])
+    assert tx_res["result"] == EXPECTED_STRUCT_TRACER, ""
 
-    # tx_res = eth_rpc.make_request(
-    #     "debug_traceCall", [tx, "latest", {"tracer": "callTracer"}]
-    # )
-    # print(tx_res)
-    # assert tx_res["result"] == EXPECTED_CALLTRACERS, ""
+    tx_res = eth_rpc.make_request(
+        "debug_traceCall", [tx, "latest", {"tracer": "callTracer"}]
+    )
+    assert tx_res["result"] == EXPECTED_CALLTRACERS, ""
 
-    # tx_res = eth_rpc.make_request(
-    #     "debug_traceTransaction",
-    #     [tx, {"tracer": "callTracer", "tracerConfig": "{'onlyTopCall':True}"}],
-    # )
-    # assert tx_res["result"] == EXPECTED_CALLTRACERS, ""
+    tx_res = eth_rpc.make_request(
+        "debug_traceCall",
+        [tx, "latest", {"tracer": "callTracer", "tracerConfig": "{'onlyTopCall':True}"}],
+    )
+    assert tx_res["result"] == EXPECTED_CALLTRACERS, ""
 
-    # tx_hash = send_transaction(w3, tx, KEYS["validator"])["transactionHash"].hex()
-
-    # tx_res = eth_rpc.make_request("debug_traceTransaction", [tx_hash])
-    # assert tx_res["result"] == EXPECTED_STRUCT_TRACER, ""
-
-    # tx_res = eth_rpc.make_request(
-    #     "debug_traceTransaction", [tx_hash, {"tracer": "callTracer"}]
-    # )
-    # assert tx_res["result"] == EXPECTED_CALLTRACERS, ""
-
-    # tx_res = eth_rpc.make_request(
-    #     "debug_traceTransaction",
-    #     [tx_hash, {"tracer": "callTracer", "tracerConfig": "{'onlyTopCall':True}"}],
-    # )
-    # assert tx_res["result"] == EXPECTED_CALLTRACERS, ""
-
-    # tx = create_contract_transaction(w3, CONTRACTS["TestERC20A"])
-
-    # tx_res = eth_rpc.make_request(
-    #     "debug_traceCall", [tx, "latest", {"tracer": "callTracer"}]
-    # )
-    # print(tx_res)
-    # tx_res["result"]["to"] = EXPECTED_CONTRACT_CREATE_TRACER["to"]
-    # assert tx_res["result"] == EXPECTED_CONTRACT_CREATE_TRACER, ""
-
-    # _, tx = send_contract_transaction(
-    #     w3, CONTRACTS["TestERC20A"], tx,
-    # )
-    # tx_hash = tx["transactionHash"].hex()
-
-    # w3_wait_for_new_blocks(w3, 1)
-
-    # tx_res = eth_rpc.make_request(
-    #     "debug_traceTransaction", [tx_hash, {"tracer": "callTracer"}]
-    # )
-    # tx_res["result"]["to"] = EXPECTED_CONTRACT_CREATE_TRACER["to"]
-    # assert tx_res["result"] == EXPECTED_CONTRACT_CREATE_TRACER, ""
 
     # bigramTracer
     # https://geth.ethereum.org/docs/developers/evm-tracing/built-in-tracers#js-tracers
+    contract, _ = deploy_contract(w3, CONTRACTS["Greeter"])
+    w3_wait_for_new_blocks(w3, 1, sleep=0.1)
 
-def test_debug_tracecall_prestate_tracer(ethermint: Ethermint):
+    topic = Web3.keccak(text="ChangeGreeting(address,string)")
+    tx = contract.functions.setGreeting("world").build_transaction()
+
+    tx = {
+        "from": from_addr,
+        "to": contract.address,
+        "data": tx["data"],
+    }
+
+    tx_res = eth_rpc.make_request("debug_traceCall", [tx, "latest", { "tracer": 'bigramTracer' }])
+    assert "result" in tx_res
+    assert tx_res["result"]['ADD-ADD'] == 2, ""
+    assert tx_res["result"]['ADD-PUSH1'] == 6, ""
+
+    
+
+
+def test_tracecall_struct_tracer(ethermint):
+    w3 = ethermint.w3
+    eth_rpc = w3.provider 
+
+    from_addr = ADDRS["signer1"]
+    to_addr = ADDRS["signer2"]
+    tx = {
+        "from": from_addr,
+        "to": to_addr,
+        "value": hex(100),
+        "gas": hex(21000),
+    }
+
+    from_bal = hex(w3.eth.get_balance(from_addr))
+    to_bal = hex(w3.eth.get_balance(to_addr))
+
+
+
+def test_tracecall_prestate_tracer(ethermint: Ethermint):
     w3 = ethermint.w3
     eth_rpc = w3.provider
 
