maalchain/maalchain_l1

commit ec8d73f585a9a8d97e3e17edc92d81623f4d89d6

feat: debug_traceCall RPC (#390)
* feat: debug_traceCall RPC

* fix unit test

* fix unit test

* add comment

* fix integration test

---------

Co-authored-by: HuangYi <huang@crypto.com>

x/evm/keeper/grpc_query.go
@@ -267,7 +267,7 @@ func (k Keeper) EthCall(c context.Context, req *types.EthCallRequest) (*types.Ms
 	txConfig := statedb.NewEmptyTxConfig(common.BytesToHash(ctx.HeaderHash()))
 
 	// pass false to not commit StateDB
-	res, err := k.ApplyMessageWithConfig(ctx, msg, nil, false, cfg, txConfig, &overrides)
+	res, err := k.ApplyMessageWithConfig(ctx, msg, nil, false, cfg, txConfig, &overrides, false)
 	if err != nil {
 		return nil, status.Error(codes.Internal, err.Error())
 	}
@@ -362,7 +362,7 @@ func (k Keeper) EstimateGas(c context.Context, req *types.EthCallRequest) (*type
 		}
 
 		// pass false to not commit StateDB
-		rsp, err = k.ApplyMessageWithConfig(ctx, msg, nil, false, cfg, txConfig, nil)
+		rsp, err = k.ApplyMessageWithConfig(ctx, msg, nil, false, cfg, txConfig, nil, false)
 		if err != nil {
 			if errors.Is(err, core.ErrIntrinsicGas) {
 				return true, nil, nil // Special case, raise gas limit
@@ -444,7 +444,7 @@ func (k Keeper) TraceTx(c context.Context, req *types.QueryTraceTxRequest) (*typ
 		}
 		txConfig.TxHash = ethTx.Hash()
 		txConfig.TxIndex = uint(i)
-		rsp, err := k.ApplyMessageWithConfig(ctx, *msg, types.NewNoOpTracer(), true, cfg, txConfig, nil)
+		rsp, err := k.ApplyMessageWithConfig(ctx, *msg, types.NewNoOpTracer(), true, cfg, txConfig, nil, true)
 		if err != nil {
 			continue
 		}
@@ -535,7 +535,179 @@ func (k Keeper) TraceBlock(c context.Context, req *types.QueryTraceBlockRequest)
 	}, nil
 }
 
-// traceTx do trace on one transaction, it returns a tuple: (traceResult, nextLogIndex, error).
+// TraceCall configures a new tracer according to the provided configuration, and
+// executes the given call in the provided environment. The return value will
+// be tracer dependent.
+func (k Keeper) TraceCall(c context.Context, req *types.QueryTraceCallRequest) (*types.QueryTraceCallResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "empty request")
+	}
+
+	if req.TraceConfig != nil && req.TraceConfig.Limit < 0 {
+		return nil, status.Errorf(codes.InvalidArgument, "output limit cannot be negative, got %d", req.TraceConfig.Limit)
+	}
+
+	// get the context of block beginning
+	contextHeight := req.BlockNumber
+	if contextHeight < 1 {
+		// 0 is a special value in `ContextWithHeight`
+		contextHeight = 1
+	}
+
+	ctx := sdk.UnwrapSDKContext(c)
+	ctx = ctx.WithBlockHeight(contextHeight)
+	ctx = ctx.WithBlockTime(req.BlockTime)
+	ctx = ctx.WithHeaderHash(common.Hex2Bytes(req.BlockHash))
+
+	var args types.TransactionArgs
+	err := json.Unmarshal(req.Args, &args)
+	if err != nil {
+		return nil, status.Error(codes.InvalidArgument, err.Error())
+	}
+	chainID, err := getChainID(ctx, req.ChainId)
+	if err != nil {
+		return nil, status.Error(codes.InvalidArgument, err.Error())
+	}
+	cfg, err := k.EVMConfig(ctx, GetProposerAddress(ctx, req.ProposerAddress), chainID)
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	txConfig := statedb.NewEmptyTxConfig(common.BytesToHash(ctx.HeaderHash().Bytes()))
+
+	// ApplyMessageWithConfig expect correct nonce set in msg
+	nonce := k.GetNonce(ctx, args.GetFrom())
+	args.Nonce = (*hexutil.Uint64)(&nonce)
+
+	msg, err := args.ToMessage(req.GasCap, cfg.BaseFee)
+	if err != nil {
+		return nil, status.Error(codes.InvalidArgument, err.Error())
+	}
+
+	result, _, err := k.traceMsg(ctx, cfg, txConfig, msg, req.TraceConfig, false)
+	if err != nil {
+		// error will be returned with detail status from traceTx
+		return nil, err
+	}
+
+	resultData, err := json.Marshal(result)
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	return &types.QueryTraceCallResponse{
+		Data: resultData,
+	}, nil
+}
+
+// traceMsg do trace on one Ethereum message, it returns a tuple: (traceResult, nextLogIndex, error).
+func (k *Keeper) traceMsg(
+	ctx sdk.Context,
+	cfg *statedb.EVMConfig,
+	txConfig statedb.TxConfig,
+	msg core.Message,
+	traceConfig *types.TraceConfig,
+	commitMessage bool,
+) (*interface{}, uint, error) {
+	// Assemble the structured logger or the JavaScript tracer
+	var (
+		tracer    tracers.Tracer
+		overrides *ethparams.ChainConfig
+		err       error
+		timeout   = defaultTraceTimeout
+	)
+
+	if traceConfig == nil {
+		traceConfig = &types.TraceConfig{}
+	}
+
+	if traceConfig.Overrides != nil {
+		overrides = traceConfig.Overrides.EthereumConfig(cfg.ChainConfig.ChainID)
+	}
+
+	logConfig := logger.Config{
+		EnableMemory:     traceConfig.EnableMemory,
+		DisableStorage:   traceConfig.DisableStorage,
+		DisableStack:     traceConfig.DisableStack,
+		EnableReturnData: traceConfig.EnableReturnData,
+		Debug:            traceConfig.Debug,
+		Limit:            int(traceConfig.Limit),
+		Overrides:        overrides,
+	}
+
+	tracer = logger.NewStructLogger(&logConfig)
+
+	txIndex, err := ethermint.SafeInt(txConfig.TxIndex)
+	if err != nil {
+		return nil, 0, status.Error(codes.Internal, err.Error())
+	}
+
+	tCtx := &tracers.Context{
+		BlockHash: txConfig.BlockHash,
+		TxIndex:   txIndex,
+		TxHash:    txConfig.TxHash,
+	}
+	if traceConfig.Tracer != "" {
+		var cfg json.RawMessage
+		if traceConfig.TracerJsonConfig != "" {
+			cfg = json.RawMessage(traceConfig.TracerJsonConfig)
+		}
+		if tracer, err = tracers.DefaultDirectory.New(traceConfig.Tracer, tCtx, cfg); err != nil {
+			return nil, 0, status.Error(codes.Internal, err.Error())
+		}
+	}
+
+	// Define a meaningful timeout of a single transaction trace
+	if traceConfig.Timeout != "" {
+		if timeout, err = time.ParseDuration(traceConfig.Timeout); err != nil {
+			return nil, 0, status.Errorf(codes.InvalidArgument, "timeout value: %s", err.Error())
+		}
+	}
+
+	// Handle timeouts and RPC cancellations
+	deadlineCtx, cancel := context.WithTimeout(ctx.Context(), timeout)
+	defer cancel()
+
+	go func() {
+		<-deadlineCtx.Done()
+		if errors.Is(deadlineCtx.Err(), context.DeadlineExceeded) {
+			tracer.Stop(errors.New("execution timeout"))
+		}
+	}()
+
+	var stateOverrides rpctypes.StateOverride
+	if traceConfig.StateOverrides != nil {
+		config, err := json.Marshal(traceConfig.StateOverrides)
+		if err != nil {
+			return nil, 0, status.Error(codes.InvalidArgument, err.Error())
+		}
+
+		if err := json.Unmarshal(config, &stateOverrides); err != nil {
+			return nil, 0, status.Error(codes.InvalidArgument, err.Error())
+		}
+	}
+
+	res, err := k.ApplyMessageWithConfig(ctx, msg, tracer, commitMessage, cfg, txConfig, &stateOverrides, true)
+	if err != nil {
+		return nil, 0, status.Error(codes.Internal, err.Error())
+	}
+
+	if res.VmError != "" {
+		if res.VmError != vm.ErrExecutionReverted.Error() {
+			return nil, 0, status.Error(codes.Internal, res.VmError)
+		}
+	}
+
+	var result interface{}
+	result, err = tracer.GetResult()
+	if err != nil {
+		return nil, 0, status.Error(codes.Internal, err.Error())
+	}
+
+	return &result, txConfig.LogIndex + uint(len(res.Logs)), nil
+}
+
+// traceMsg do trace on one Ethereum message, it returns a tuple: (traceResult, nextLogIndex, error).
 func (k *Keeper) traceTx(
 	ctx sdk.Context,
 	cfg *statedb.EVMConfig,
@@ -610,9 +782,27 @@ func (k *Keeper) traceTx(
 		}
 	}()
 
-	res, err := k.ApplyMessageWithConfig(ctx, *msg, tracer, commitMessage, cfg, txConfig, nil)
+	var stateOverrides rpctypes.StateOverride
+	if traceConfig.StateOverrides != nil {
+		config, err := json.Marshal(traceConfig.StateOverrides)
+		if err != nil {
+			return nil, 0, status.Error(codes.InvalidArgument, err.Error())
+		}
+
+		if err := json.Unmarshal(config, &stateOverrides); err != nil {
+			return nil, 0, status.Error(codes.InvalidArgument, err.Error())
+		}
+	}
+
+	res, err := k.ApplyMessageWithConfig(ctx, *msg, tracer, commitMessage, cfg, txConfig, &stateOverrides, true)
 	if err != nil {
 		return nil, 0, status.Error(codes.Internal, err.Error())
+	}
+
+	if res.VmError != "" {
+		if res.VmError != vm.ErrExecutionReverted.Error() {
+			return nil, 0, status.Error(codes.Internal, res.VmError)
+		}
 	}
 
 	var result interface{}

tests/integration_tests/test_tracers.py
@@ -9,6 +9,7 @@ from .expected_constants import (
     EXPECTED_CONTRACT_CREATE_TRACER,
     EXPECTED_STRUCT_TRACER,
 )
+from .network import Ethermint
 from .utils import (
     ADDRS,
     CONTRACTS,
@@ -19,10 +20,11 @@ from .utils import (
 )
 
 
-def test_tracers(ethermint_rpc_ws):
+def test_trace_transactions_tracers(ethermint_rpc_ws):
     w3: Web3 = ethermint_rpc_ws.w3
     eth_rpc = w3.provider
     gas_price = w3.eth.gas_price
+
     tx = {"to": ADDRS["community"], "value": 100, "gasPrice": gas_price}
     tx_hash = send_transaction(w3, tx)["transactionHash"].hex()
     method = "debug_traceTransaction"
@@ -38,6 +40,7 @@ def test_tracers(ethermint_rpc_ws):
     assert tx_res["result"] == EXPECTED_CALLTRACERS, ""
     _, tx = deploy_contract(w3, CONTRACTS["TestERC20A"])
     tx_hash = tx["transactionHash"].hex()
+
     w3_wait_for_new_blocks(w3, 1)
     tx_res = eth_rpc.make_request(method, [tx_hash, tracer])
     tx_res["result"]["to"] = EXPECTED_CONTRACT_CREATE_TRACER["to"]
@@ -77,9 +80,377 @@ def test_crosscheck(ethermint, geth):
 
     providers = [ethermint.w3, geth.w3]
     with ThreadPoolExecutor(len(providers)) as exec:
-        tasks = [
-            exec.submit(process, w3) for w3 in providers
-        ]
+        tasks = [exec.submit(process, w3) for w3 in providers]
         res = [future.result() for future in as_completed(tasks)]
         assert len(res) == len(providers)
         assert res[0] == res[1], res
+
+
+def test_tracecall_insufficient_funds(ethermint_rpc_ws):
+    w3: Web3 = ethermint_rpc_ws.w3
+    eth_rpc = w3.provider
+    gas_price = w3.eth.gas_price
+
+    # Insufficient funds
+    tx = {
+        # an non-exist address
+        "from": "0x1000000000000000000000000000000000000000",
+        "to": ADDRS["community"],
+        "value": hex(100),
+        "gasPrice": hex(gas_price),
+        "gas": hex(21000),
+    }
+    tx_res = eth_rpc.make_request(
+        "debug_traceCall", [tx, "latest", {"tracer": "prestateTracer"}]
+    )
+    assert "error" in tx_res
+    assert tx_res["error"] == {
+        "code": -32000,
+        "message": "rpc error: code = Internal desc = insufficient balance for transfer",  # noqa: E501
+    }, ""
+
+    tx_res = eth_rpc.make_request(
+        "debug_traceCall", [tx, "latest", {"tracer": "callTracer"}]
+    )
+    assert "error" in tx_res
+    assert tx_res["error"] == {
+        "code": -32000,
+        "message": "rpc error: code = Internal desc = insufficient balance for transfer",  # noqa: #E501
+    }, ""
+
+    from_addr = ADDRS["validator"]
+    to_addr = ADDRS["community"]
+    tx = {
+        "from": from_addr,
+        "to": to_addr,
+        "value": hex(100),
+        "gas": hex(21000),
+    }
+
+    tx_res = eth_rpc.make_request("debug_traceCall", [tx, "latest"])
+    assert tx_res["result"] == EXPECTED_STRUCT_TRACER, ""
+
+    tx_res = eth_rpc.make_request(
+        "debug_traceCall", [tx, "latest", {"tracer": "callTracer"}]
+    )
+    assert tx_res["result"] == EXPECTED_CALLTRACERS, ""
+
+    tx_res = eth_rpc.make_request(
+        "debug_traceCall",
+        [
+            tx,
+            "latest",
+            {"tracer": "callTracer", "tracerConfig": {'onlyTopCall': True}},
+        ],
+    )
+    assert tx_res["result"] == EXPECTED_CALLTRACERS, ""
+
+
+def test_js_tracers(ethermint):
+    w3: Web3 = ethermint.w3
+    eth_rpc = w3.provider
+
+    from_addr = ADDRS["validator"]
+
+    contract, _ = deploy_contract(w3, CONTRACTS["Greeter"])
+    w3_wait_for_new_blocks(w3, 1, sleep=0.1)
+
+    tx = contract.functions.setGreeting("world").build_transaction()
+
+    tx = {
+        "from": from_addr,
+        "to": contract.address,
+        "data": tx["data"],
+    }
+
+    # bigramTracer
+    # https://geth.ethereum.org/docs/developers/evm-tracing/built-in-tracers#js-tracers
+    tx_res = eth_rpc.make_request(
+        "debug_traceCall", [tx, "latest", {"tracer": "bigramTracer"}]
+    )
+    assert "result" in tx_res
+    tx_res = tx_res["result"]
+    assert tx_res["ADD-ADD"] == 2
+    assert tx_res["ADD-PUSH1"] == 6
+
+    # evmdis
+    tx_res = eth_rpc.make_request(
+        "debug_traceCall", [tx, "latest", {"tracer": "evmdisTracer"}]
+    )
+    assert "result" in tx_res
+    tx_res = tx_res["result"]
+    assert tx_res[0] == {"depth": 1, "len": 2, "op": 96, "result": ["80"]}
+
+    # opcount
+    tx_res = eth_rpc.make_request(
+        "debug_traceCall", [tx, "latest", {"tracer": "opcountTracer"}]
+    )
+    assert "result" in tx_res
+    tx_res = tx_res["result"]
+    assert tx_res > 0
+
+    # trigram
+    tx_res = eth_rpc.make_request(
+        "debug_traceCall", [tx, "latest", {"tracer": "trigramTracer"}]
+    )
+    assert "result" in tx_res
+    tx_res = tx_res["result"]
+    assert tx_res["ADD-ADD-MSTORE"] == 1
+    assert tx_res["DUP2-MLOAD-DUP1"] == 1
+
+    # unigram
+    tx_res = eth_rpc.make_request(
+        "debug_traceCall", [tx, "latest", {"tracer": "unigramTracer"}]
+    )
+    assert "result" in tx_res
+    tx_res = tx_res["result"]
+    assert tx_res["POP"] == 24
+
+
+def test_custom_js_tracers(ethermint):
+    w3: Web3 = ethermint.w3
+    eth_rpc = w3.provider
+
+    from_addr = ADDRS["validator"]
+
+    contract, _ = deploy_contract(w3, CONTRACTS["Greeter"])
+    w3_wait_for_new_blocks(w3, 1, sleep=0.1)
+
+    tx = contract.functions.setGreeting("world").build_transaction()
+
+    tx = {
+        "from": from_addr,
+        "to": contract.address,
+        "data": tx["data"],
+    }
+
+    tracer = """{
+        data: [],
+        fault: function(log) {},
+        step: function(log) {
+            if(log.op.toString() == "POP") this.data.push(log.stack.peek(0));
+        },
+        result: function() { return this.data; }
+    }"""
+    tx_res = eth_rpc.make_request("debug_traceCall", [tx, "latest", {"tracer": tracer}])
+    assert "result" in tx_res
+    tx_res = tx_res["result"]
+
+    tracer = """{
+        retVal: [],
+        step: function(log,db) {
+            this.retVal.push(log.getPC() + ":" + log.op.toString())
+        },
+        fault: function(log,db) {
+            this.retVal.push("FAULT: " + JSON.stringify(log))
+        },
+        result: function(ctx,db) {
+            return this.retVal
+        }
+    }
+    """
+    tx_res = eth_rpc.make_request("debug_traceCall", [tx, "latest", {"tracer": tracer}])
+    assert "result" in tx_res
+    tx_res = tx_res["result"]
+    assert tx_res[0] == "0:PUSH1"
+
+
+def test_tracecall_struct_tracer(ethermint: Ethermint):
+    w3 = ethermint.w3
+    eth_rpc = w3.provider
+
+    # set gas limit in tx
+    from_addr = ADDRS["signer1"]
+    to_addr = ADDRS["signer2"]
+    tx = {
+        "from": from_addr,
+        "to": to_addr,
+        "value": hex(100),
+        "gas": hex(21000),
+    }
+
+    tx_res = eth_rpc.make_request("debug_traceCall", [tx, "latest"])
+    assert tx_res["result"] == EXPECTED_STRUCT_TRACER, ""
+
+    # no gas limit set in tx
+    # default GasCap in ethermint
+    gas_cap = 25000000
+
+    tx = {
+        "from": from_addr,
+        "to": to_addr,
+        "value": hex(100),
+    }
+
+    tx_res = eth_rpc.make_request("debug_traceCall", [tx, "latest"])
+    assert tx_res["result"] == {
+        "failed": False,
+        "gas": gas_cap / 2,
+        "returnValue": "",
+        "structLogs": [],
+    }
+
+
+def test_tracecall_prestate_tracer(ethermint: Ethermint):
+    w3 = ethermint.w3
+    eth_rpc = w3.provider
+
+    sender = ADDRS["signer1"]
+    receiver = ADDRS["signer2"]
+
+    # make a transaction make sure the nonce is not 0
+    w3.eth.send_transaction(
+        {
+            "from": sender,
+            "to": receiver,
+            "value": hex(1),
+        }
+    )
+
+    w3.eth.send_transaction(
+        {
+            "from": receiver,
+            "to": sender,
+            "value": hex(1),
+        }
+    )
+    w3_wait_for_new_blocks(w3, 1, sleep=0.1)
+
+    sender_nonce = w3.eth.get_transaction_count(sender)
+    sender_bal = w3.eth.get_balance(sender)
+    receiver_nonce = w3.eth.get_transaction_count(receiver)
+    receiver_bal = w3.eth.get_balance(receiver)
+
+    tx = {
+        "from": sender,
+        "to": receiver,
+        "value": hex(1),
+    }
+    w3_wait_for_new_blocks(w3, 1, sleep=0.1)
+    tx_res = eth_rpc.make_request(
+        "debug_traceCall", [tx, "latest", {"tracer": "prestateTracer"}]
+    )
+
+    assert "result" in tx_res
+    assert tx_res["result"][sender.lower()] == {
+        "balance": hex(sender_bal),
+        "nonce": sender_nonce,
+    }
+    assert tx_res["result"][receiver.lower()] == {
+        "balance": hex(receiver_bal),
+        "nonce": receiver_nonce,
+    }
+
+
+def test_debug_tracecall_call_tracer(ethermint_rpc_ws):
+    w3: Web3 = ethermint_rpc_ws.w3
+    eth_rpc = w3.provider
+
+    tx = {
+        "from": ADDRS["signer1"],
+        "to": ADDRS["signer2"],
+        "value": hex(1),
+        "gas": hex(21000),
+    }
+
+    tx_res = eth_rpc.make_request(
+        "debug_traceCall", [tx, "latest", {"tracer": "callTracer"}]
+    )
+
+    assert "result" in tx_res
+    assert tx_res["result"] == {
+        "type": "CALL",
+        "from": ADDRS["signer1"].lower(),
+        "to": ADDRS["signer2"].lower(),
+        "value": hex(1),
+        "gas": hex(21000),
+        "gasUsed": hex(21000),
+        "input": "0x",
+    }
+
+    # no gas limit set in tx
+    tx = {
+        "from": ADDRS["signer1"],
+        "to": ADDRS["signer2"],
+        "value": hex(1),
+    }
+
+    tx_res = eth_rpc.make_request(
+        "debug_traceCall", [tx, "latest", {"tracer": "callTracer"}]
+    )
+
+    gas_cap = 25000000
+
+    assert "result" in tx_res
+    assert tx_res["result"] == {
+        "type": "CALL",
+        "from": ADDRS["signer1"].lower(),
+        "to": ADDRS["signer2"].lower(),
+        "value": hex(1),
+        "gas": hex(gas_cap),
+        "gasUsed": hex(int(gas_cap / 2)),
+        "input": "0x",
+    }
+
+
+def test_debug_tracecall_state_overrides(ethermint_rpc_ws):
+    w3: Web3 = ethermint_rpc_ws.w3
+    eth_rpc = w3.provider
+
+    # generate random address, set balance in stateOverrides,
+    # use prestateTracer to check balance
+    balance = "0xffffffff"
+
+    address = w3.eth.account.create().address
+
+    tx = {
+        "from": address,
+        "to": ADDRS["signer2"],
+        "value": hex(1),
+    }
+
+    config = {
+        "tracer": "prestateTracer",
+        "stateOverrides": {
+            address: {
+                "balance": balance,
+            },
+        },
+    }
+
+    tx_res = eth_rpc.make_request("debug_traceCall", [tx, "latest", config])
+
+    assert "result" in tx_res
+    tx_res = tx_res["result"]
+    assert tx_res[address.lower()]["balance"] == balance
+
+
+def test_debug_tracecall_return_revert_data_when_call_failed(ethermint):
+    w3: Web3 = ethermint.w3
+    eth_rpc = w3.provider
+
+    test_revert, _ = deploy_contract(
+        w3,
+        CONTRACTS["TestRevert"],
+    )
+
+    w3_wait_for_new_blocks(w3, 1, sleep=0.1)
+
+    tx_res = eth_rpc.make_request(
+        "debug_traceCall",
+        [
+            {
+                "value": "0x0",
+                "to": test_revert.address,
+                "from": ADDRS["validator"],
+                "data": "0x9ffb86a5",
+            },
+            "latest",
+        ],
+    )
+    assert "result" in tx_res
+    tx_res = tx_res["result"]
+    assert (
+        tx_res["returnValue"]
+        == "08c379a00000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000001a46756e6374696f6e20686173206265656e207265766572746564000000000000"  # noqa: E501
+    )

app/app.go
@@ -923,7 +923,7 @@ func (app *EthermintApp) RegisterNodeService(clientCtx client.Context) {
 }
 
 // RegisterSwaggerAPI registers swagger route with API Server
-func RegisterSwaggerAPI(ctx client.Context, rtr *mux.Router) {
+func RegisterSwaggerAPI(_ client.Context, rtr *mux.Router) {
 	root, err := fs.Sub(docs.SwaggerUI, "swagger-ui")
 	if err != nil {
 		panic(err)

proto/ethermint/evm/v1/query.proto
@@ -71,6 +71,11 @@ service Query {
     option (google.api.http).get = "/ethermint/evm/v1/trace_block";
   }
 
+  // TraceCall implements the `debug_traceCall` rpc api
+  rpc TraceCall(QueryTraceCallRequest) returns (QueryTraceCallResponse) {
+    option (google.api.http).get = "/ethermint/evm/v1/trace_call";
+  }
+
   // BaseFee queries the base fee of the parent block of the current block,
   // it's similar to feemarket module's method, but also checks london hardfork status.
   rpc BaseFee(QueryBaseFeeRequest) returns (QueryBaseFeeResponse) {
@@ -268,6 +273,32 @@ message QueryTraceTxRequest {
 
 // QueryTraceTxResponse defines TraceTx response
 message QueryTraceTxResponse {
+  // data is the response serialized in bytes
+  bytes data = 1;
+}
+
+// QueryTraceCallRequest defines TraceCall request
+message QueryTraceCallRequest {
+  // args uses the same json format as the json rpc api.
+  bytes args = 1;
+  // gas_cap defines the default gas cap to be used
+  uint64 gas_cap = 2;
+  // proposer_address of the requested block in hex format
+  bytes proposer_address = 3 [(gogoproto.casttype) = "github.com/cosmos/cosmos-sdk/types.ConsAddress"];
+  // trace_config holds extra parameters to trace functions.
+  TraceConfig trace_config = 4;
+  // block_number of requested transaction
+  int64 block_number = 5;
+  // block_hash of requested transaction
+  string block_hash = 6;
+  // block_time of requested transaction
+  google.protobuf.Timestamp block_time = 7 [(gogoproto.nullable) = false, (gogoproto.stdtime) = true];
+  // chain_id is the the eip155 chain id parsed from the requested block header
+  int64 chain_id = 8;
+}
+
+// QueryTraceCallResponse defines TraceCallResponse
+message QueryTraceCallResponse {
   // data is the response serialized in bytes
   bytes data = 1;
 }

proto/ethermint/evm/v1/trace_config_v0.proto
@@ -6,6 +6,17 @@ import "ethermint/evm/v1/chain_config_v0.proto";
 
 option go_package = "github.com/evmos/ethermint/x/evm/types";
 
+
+// OverrideAccount indicates the overriding fields of account during the execution of
+// a message call.
+message V0OverrideAccount {
+  uint64 nonce  = 1;
+  string code = 2;
+  string balance = 3;
+  map<string, string> state = 4;
+  map<string, string> state_diff = 5 [(gogoproto.jsontag) = "stateDiff"];
+}
+
 // V0TraceConfig holds extra parameters to trace functions.
 message V0TraceConfig {
   // DEPRECATED: DisableMemory and DisableReturnData have been renamed to
@@ -36,4 +47,6 @@ message V0TraceConfig {
   bool enable_return_data = 12 [(gogoproto.jsontag) = "enableReturnData"];
   // tracer_json_config configures the tracer using a JSON string
   string tracer_json_config = 13 [(gogoproto.jsontag) = "tracerConfig"];
+  // temporary state modifications to Geth in order to simulate the effects of eth_call
+  map<string, V0OverrideAccount> state_overrides = 14 [(gogoproto.jsontag) = "stateOverrides"];
 }

rpc/backend/backend.go
@@ -139,6 +139,7 @@ type EVMBackend interface {
 	// Tracing
 	TraceTransaction(hash common.Hash, config *rpctypes.TraceConfig) (interface{}, error)
 	TraceBlock(height rpctypes.BlockNumber, config *rpctypes.TraceConfig, block *tmrpctypes.ResultBlock) ([]*evmtypes.TxTraceResult, error)
+	TraceCall(args evmtypes.TransactionArgs, blockNr rpctypes.BlockNumberOrHash, config *rpctypes.TraceConfig) (interface{}, error)
 }
 
 var _ BackendI = (*Backend)(nil)

rpc/backend/evm_query_client_test.go
@@ -273,3 +273,15 @@ func RegisterBalanceError(queryClient *mocks.EVMQueryClient, addr common.Address
 	queryClient.On("Balance", rpc.ContextWithHeight(height), &evmtypes.QueryBalanceRequest{Address: addr.String()}).
 		Return(nil, errortypes.ErrInvalidRequest)
 }
+
+// TraceCall
+func RegisterTraceCall(queryClient *mocks.EVMQueryClient, request *evmtypes.QueryTraceCallRequest, response *evmtypes.QueryTraceCallResponse) {
+	queryClient.On("TraceCall", rpc.ContextWithHeight(request.BlockNumber), request).
+		Return(response, nil)
+}
+
+func RegisterTraceCallError(queryClient *mocks.EVMQueryClient, request *evmtypes.QueryTraceCallRequest) {
+	ctx, _ := context.WithCancel(rpc.ContextWithHeight(1))
+	queryClient.On("TraceCall", ctx, request).
+		Return(nil, errortypes.ErrInvalidRequest)
+}

rpc/backend/mocks/evm_query_client.go
@@ -347,6 +347,36 @@ func (_m *EVMQueryClient) TraceTx(ctx context.Context, in *types.QueryTraceTxReq
 	return r0, r1
 }
 
+// TraceCall provides a mock function with given fields: ctx, in, opts
+func (_m *EVMQueryClient) TraceCall(ctx context.Context, in *types.QueryTraceCallRequest, opts ...grpc.CallOption) (*types.QueryTraceCallResponse, error) {
+	_va := make([]interface{}, len(opts))
+	for _i := range opts {
+		_va[_i] = opts[_i]
+	}
+	var _ca []interface{}
+	_ca = append(_ca, ctx, in)
+	_ca = append(_ca, _va...)
+	ret := _m.Called(_ca...)
+
+	var r0 *types.QueryTraceCallResponse
+	if rf, ok := ret.Get(0).(func(context.Context, *types.QueryTraceCallRequest, ...grpc.CallOption) *types.QueryTraceCallResponse); ok {
+		r0 = rf(ctx, in, opts...)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*types.QueryTraceCallResponse)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(context.Context, *types.QueryTraceCallRequest, ...grpc.CallOption) error); ok {
+		r1 = rf(ctx, in, opts...)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // ValidatorAccount provides a mock function with given fields: ctx, in, opts
 func (_m *EVMQueryClient) ValidatorAccount(ctx context.Context, in *types.QueryValidatorAccountRequest, opts ...grpc.CallOption) (*types.QueryValidatorAccountResponse, error) {
 	_va := make([]interface{}, len(opts))

crypto-org-chain/ethermint

commit ba3039a48c410310aa3d90464a6dd8f654c0ff2e

Problem: trace response for failed tx is not aligned with go-ethereum
this bug was introduced in https://github.com/crypto-org-chain/ethermint/commit/ec8d73f585a9a8d97e3e17edc92d81623f4d89d6#

x/evm/keeper/grpc_query.go
@@ -716,7 +716,7 @@ func (k *Keeper) prepareTrace(
 	}
 
 	if res.VmError != "" {
-		if res.VmError != vm.ErrExecutionReverted.Error() {
+		if res.VmError != vm.ErrExecutionReverted.Error() && res.VmError != vm.ErrOutOfGas.Error() {
 			return nil, 0, status.Error(codes.Internal, res.VmError)
 		}
 	}

tests/integration_tests/test_tracers.py
@@ -24,6 +24,37 @@ from .utils import (
 )
 
 
+def test_trace_error(ethermint, geth):
+    method = "debug_traceTransaction"
+    tracer = {"tracer": "callTracer"}
+    tracers = [[tracer]]
+    iterations = 1
+    acc = derive_random_account()
+
+    def process(w3):
+        # fund new sender to deploy contract with same address
+        fund_acc(w3, acc)
+        contract, _ = deploy_contract(w3, CONTRACTS["TestMessageCall"], key=acc.key)
+        tx = contract.functions.test(iterations).build_transaction({"gas": 21204})
+        tx_hash = send_transaction(w3, tx)["transactionHash"].hex()
+        res = []
+        call = w3.provider.make_request
+        with ThreadPoolExecutor(len(tracers)) as exec:
+            params = [([tx_hash] + cfg) for cfg in tracers]
+            exec_map = exec.map(call, itertools.repeat(method), params)
+            for resp in exec_map:
+                assert "out of gas" in resp["result"]["error"], resp
+                res = [json.dumps(resp["result"], sort_keys=True)]
+        return res
+
+    providers = [ethermint.w3, geth.w3]
+    with ThreadPoolExecutor(len(providers)) as exec:
+        tasks = [exec.submit(process, w3) for w3 in providers]
+        res = [future.result() for future in as_completed(tasks)]
+        assert len(res) == len(providers)
+        assert res[0] == res[-1], res
+
+
 def test_trace_transactions_tracers(ethermint, geth):
     method = "debug_traceTransaction"
     tracer = {"tracer": "callTracer"}