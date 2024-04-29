b9ae738e98504c333495537a7f2df597f10752d2
mmsqe <mavis@crypto.com>
2024-04-29 17:52:38 +0800

    Problem: no parsed log from old event

--- x/evm/types/utils.go
+++ x/evm/types/utils.go
@@ -17,9 +17,11 @@ package types
 
 import (
 	"encoding/hex"
+	"encoding/json"
 	"fmt"
 	"math/big"
 
+	abci "github.com/cometbft/cometbft/abci/types"
 	"github.com/cosmos/gogoproto/proto"
 	feemarkettypes "github.com/evmos/ethermint/x/feemarket/types"
 
@@ -97,7 +99,7 @@ func logsFromTxResponse(dst []*ethtypes.Log, rsp *MsgEthereumTxResponse, blockNu
 }
 
 // DecodeMsgLogsFromEvents decodes a protobuf-encoded byte slice into ethereum logs, for a single message.
-func DecodeMsgLogsFromEvents(in []byte, msgIndex int, blockNumber uint64) ([]*ethtypes.Log, error) {
+func DecodeMsgLogsFromEvents(in []byte, events []abci.Event, msgIndex int, blockNumber uint64) ([]*ethtypes.Log, error) {
 	txResponses, err := DecodeTxResponses(in)
 	if err != nil {
 		return nil, err
@@ -107,7 +109,51 @@ func DecodeMsgLogsFromEvents(in []byte, msgIndex int, blockNumber uint64) ([]*et
 		return nil, fmt.Errorf("invalid message index: %d", msgIndex)
 	}
 
-	return logsFromTxResponse(nil, txResponses[msgIndex], blockNumber), nil
+	logs, err := logsFromTxResponse(nil, txResponses[msgIndex], blockNumber), nil
+	if err != nil {
+		return nil, err
+	}
+
+	if len(logs) == 0 {
+		logs, err = TxLogsFromEvents(events, msgIndex)
+	}
+	return logs, err
+}
+
+// TxLogsFromEvents parses ethereum logs from cosmos events for specific msg index
+func TxLogsFromEvents(events []abci.Event, msgIndex int) ([]*ethtypes.Log, error) {
+	for _, event := range events {
+		if event.Type != EventTypeTxLog {
+			continue
+		}
+
+		if msgIndex > 0 {
+			// not the eth tx we want
+			msgIndex--
+			continue
+		}
+
+		return ParseTxLogsFromEvent(event)
+	}
+	return nil, fmt.Errorf("eth tx logs not found for message index %d", msgIndex)
+}
+
+// ParseTxLogsFromEvent parse tx logs from one event
+func ParseTxLogsFromEvent(event abci.Event) ([]*ethtypes.Log, error) {
+	logs := make([]*Log, 0, len(event.Attributes))
+	for _, attr := range event.Attributes {
+		if attr.Key != AttributeKeyTxLog {
+			continue
+		}
+
+		var log Log
+		if err := json.Unmarshal([]byte(attr.Value), &log); err != nil {
+			return nil, err
+		}
+
+		logs = append(logs, &log)
+	}
+	return LogsToEthereum(logs), nil
 }
 
 // DecodeTxLogsFromEvents decodes a protobuf-encoded byte slice into ethereum logs
