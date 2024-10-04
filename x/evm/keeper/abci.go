5376e54a57170c1599c66e6615effaa75ac3280d
Eduard Voiculescu <eduardvoiculescu95@gmail.com>
2024-10-04 13:09:02 -0400

    Adding EndBlock tracer and using the k.evmTracer instead of the tracer from the config

--- x/evm/keeper/abci.go
+++ x/evm/keeper/abci.go
@@ -17,7 +17,6 @@ package keeper
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/tracing"
 	"github.com/ethereum/go-ethereum/core/types"
 	"math/big"
@@ -32,24 +31,18 @@ func (k *Keeper) BeginBlock(ctx sdk.Context) error {
 		return err
 	}
 
-	cfg, err := k.EVMConfig(ctx, k.ChainID(), common.Hash{})
-	if err != nil {
-		return err
-	}
-
-	if cfg.Tracer != nil {
+	if k.evmTracer != nil && k.evmTracer.OnBlockStart != nil {
 		b := types.NewBlock(&types.Header{
 			Number: big.NewInt(ctx.BlockHeight()),
 			Time:   uint64(ctx.BlockTime().Unix()),
 		}, nil, nil, nil)
 
-		cfg.Tracer.OnBlockStart(tracing.BlockEvent{
+		k.evmTracer.OnBlockStart(tracing.BlockEvent{
 			Block: b,
 			TD:    big.NewInt(1),
 			// Finalized: , // todo: how to set up the header here?
 			// Safe:      , // todo: how to set up the header here?
 		})
-
 	}
 
 	return nil
@@ -62,15 +55,11 @@ func (k *Keeper) EndBlock(ctx sdk.Context) error {
 	k.CollectTxBloom(ctx)
 	k.RemoveParamsCache(ctx)
 
-	// TODO: call EndBlock on Tracer
-	//config, err := k.EVMConfig(ctx, k.ChainID(), common.Hash{})
-	//if err != nil {
-	//	return err
-	//}
-	//
-	//defer func() {
-	//	config.Tracer.OnBlockEnd(nil)
-	//}()
+	if k.evmTracer != nil && k.evmTracer.OnBlockEnd != nil {
+		defer func() {
+			k.evmTracer.OnBlockEnd(nil)
+		}()
+	}
 
 	return nil
 }
