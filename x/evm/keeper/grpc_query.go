46f7ca8653fe6fe574540e3e19c927dc89097043
Xinyu Zhao <xinyu.zhao@crypto.com>
2025-06-19 17:19:37 +0800

    fix: fmt

--- x/evm/keeper/grpc_query.go
+++ x/evm/keeper/grpc_query.go
@@ -354,19 +354,19 @@ func (k Keeper) EstimateGas(c context.Context, req *types.EthCallRequest) (*type
 	executable := func(gas uint64) (vmError bool, rsp *types.MsgEthereumTxResponse, err error) {
 		// update the message with the new gas value
 		msg = &core.Message{
-			From:       msg.From,
-			To:         msg.To,
-			Nonce:      msg.Nonce,
-			Value:      msg.Value,
-			GasLimit:   gas,
-			GasPrice:   msg.GasPrice,
-			GasFeeCap:  msg.GasFeeCap,
-			GasTipCap:  msg.GasTipCap,
-			Data:       msg.Data,
-			AccessList: msg.AccessList,
-			BlobGasFeeCap: msg.BlobGasFeeCap,
-			BlobHashes: msg.BlobHashes,
-			SkipNonceChecks: msg.SkipNonceChecks,
+			From:             msg.From,
+			To:               msg.To,
+			Nonce:            msg.Nonce,
+			Value:            msg.Value,
+			GasLimit:         gas,
+			GasPrice:         msg.GasPrice,
+			GasFeeCap:        msg.GasFeeCap,
+			GasTipCap:        msg.GasTipCap,
+			Data:             msg.Data,
+			AccessList:       msg.AccessList,
+			BlobGasFeeCap:    msg.BlobGasFeeCap,
+			BlobHashes:       msg.BlobHashes,
+			SkipNonceChecks:  msg.SkipNonceChecks,
 			SkipFromEOACheck: msg.SkipFromEOACheck,
 		}
 
