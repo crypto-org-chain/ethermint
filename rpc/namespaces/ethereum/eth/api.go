e044ff70b72e38bba6d6795e47bb021398510bcf
Randy Ang <randy.ang@crypto.com>
2025-08-18 12:50:44 +0800

    revert removal of coinbase and pending tx json rpc

--- rpc/namespaces/ethereum/eth/api.go
+++ rpc/namespaces/ethereum/eth/api.go
@@ -108,11 +108,13 @@ type EthereumAPI interface {
 
 	// Other
 	Syncing() (interface{}, error)
+	Coinbase() (string, error)
 	Sign(address common.Address, data hexutil.Bytes) (hexutil.Bytes, error)
 	GetTransactionLogs(txHash common.Hash) ([]*ethtypes.Log, error)
 	SignTypedData(address common.Address, typedData apitypes.TypedData) (hexutil.Bytes, error)
 	FillTransaction(args evmtypes.TransactionArgs) (*rpctypes.SignTransactionResult, error)
 	Resend(ctx context.Context, args evmtypes.TransactionArgs, gasPrice *hexutil.Big, gasLimit *hexutil.Uint64) (common.Hash, error)
+	GetPendingTransactions() ([]*rpctypes.RPCTransaction, error)
 	// eth_signTransaction (on Ethereum.org)
 	// eth_getCompilers (on Ethereum.org)
 	// eth_compileSolidity (on Ethereum.org)
@@ -393,6 +395,18 @@ func (e *PublicAPI) Syncing() (interface{}, error) {
 	return e.backend.Syncing()
 }
 
+// Coinbase is the address that staking rewards will be send to (alias for Etherbase).
+func (e *PublicAPI) Coinbase() (string, error) {
+	e.logger.Debug("eth_coinbase")
+
+	coinbase, err := e.backend.GetCoinbase()
+	if err != nil {
+		return "", err
+	}
+	ethAddr := common.BytesToAddress(coinbase.Bytes())
+	return ethAddr.Hex(), nil
+}
+
 // Sign signs the provided data using the private key of address via Geth's signature standard.
 func (e *PublicAPI) Sign(address common.Address, data hexutil.Bytes) (hexutil.Bytes, error) {
 	e.logger.Debug("eth_sign", "address", address.Hex(), "data", common.Bytes2Hex(data))
@@ -478,4 +492,42 @@ func (e *PublicAPI) Resend(_ context.Context,
 ) (common.Hash, error) {
 	e.logger.Debug("eth_resend", "args", args.String())
 	return e.backend.Resend(args, gasPrice, gasLimit)
+}
+
+// GetPendingTransactions returns the transactions that are in the transaction pool
+// and have a from address that is one of the accounts this node manages.
+func (e *PublicAPI) GetPendingTransactions() ([]*rpctypes.RPCTransaction, error) {
+	e.logger.Debug("eth_getPendingTransactions")
+
+	txs, err := e.backend.PendingTransactions()
+	if err != nil {
+		return nil, err
+	}
+
+	result := make([]*rpctypes.RPCTransaction, 0, len(txs))
+	for _, tx := range txs {
+		for _, msg := range (*tx).GetMsgs() {
+			ethMsg, ok := msg.(*evmtypes.MsgEthereumTx)
+			if !ok {
+				// not valid ethereum tx
+				break
+			}
+
+			rpctx, err := rpctypes.NewTransactionFromMsg(
+				ethMsg,
+				common.Hash{},
+				uint64(0),
+				uint64(0),
+				nil,
+				e.backend.ChainConfig().ChainID,
+			)
+			if err != nil {
+				return nil, err
+			}
+
+			result = append(result, rpctx)
+		}
+	}
+
+	return result, nil
 }
