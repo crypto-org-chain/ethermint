623147c837829407bfbae37a6aa950826c95835e
Xinyu <xinyu.zhao@crypto.com>
2025-09-16 16:22:59 +0800

    fix: use new addr

--- tests/integration_tests/test_tracers.py
+++ tests/integration_tests/test_tracers.py
@@ -811,7 +811,7 @@ def test_prestate_tracer_block_miner_address(ethermint, geth):
     prestateTracer on a tx will include the block miner address
     """
     acc = ACCOUNTS["community"]
-    receiver = derive_new_account(9)
+    receiver = derive_new_account(12)
 
     def process(w3):
         assert (
