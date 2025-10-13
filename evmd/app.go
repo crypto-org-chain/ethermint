ce740e06c410b5740bb3bf715f281bf80c954dbc
Randy Ang <randy.ang@crypto.com>
2025-10-13 17:40:15 +0800

    update CHANGELOG and update cosmos-sdk

--- evmd/app.go
+++ evmd/app.go
@@ -380,6 +380,7 @@ func NewEthermintApp(
 		panic(err)
 	}
 	app.txConfig = txConfig
+	stakingCacheSize := cast.ToUint(appOpts.Get(server.FlagStakingCacheSize))
 	app.StakingKeeper = stakingkeeper.NewKeeper(
 		appCodec,
 		runtime.NewKVStoreService(keys[stakingtypes.StoreKey]),
@@ -388,7 +389,7 @@ func NewEthermintApp(
 		authAddr,
 		address.NewBech32Codec(sdk.GetConfig().GetBech32ValidatorAddrPrefix()),
 		address.NewBech32Codec(sdk.GetConfig().GetBech32ConsensusAddrPrefix()),
-		0, // TODO: should this be a flag?
+		stakingCacheSize,
 	)
 	app.MintKeeper = mintkeeper.NewKeeper(
 		appCodec,
