442183cc2f907d8f99026e0f589e954ded6ddac0
Randy Ang <randy.ang@crypto.com>
2025-12-08 12:58:11 +0800

    minor change in function name

--- x/evm/types/params.go
+++ x/evm/types/params.go
@@ -90,11 +90,11 @@ func (p Params) Validate() error {
 		return err
 	}
 
-	if err := ValidateMaxInt64(p.HeaderHashNum); err != nil {
+	if err := ValidateMaxInt64Overflow(p.HeaderHashNum); err != nil {
 		return err
 	}
 
-	if err := ValidateMaxInt64(p.HistoryServeWindow); err != nil {
+	if err := ValidateMaxInt64Overflow(p.HistoryServeWindow); err != nil {
 		return err
 	}
 
@@ -149,7 +149,7 @@ func ValidateChainConfig(i interface{}) error {
 	return cfg.Validate()
 }
 
-func ValidateMaxInt64(i interface{}) error {
+func ValidateMaxInt64Overflow(i interface{}) error {
 	num, ok := i.(uint64)
 	if !ok {
 		return fmt.Errorf("invalid parameter type: %T", i)
