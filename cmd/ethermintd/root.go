216c99ee475f2bceb191c7f1a23036f73d73d801
Randy Ang <randy.ang@crypto.com>
2026-02-07 00:54:38 +0800

    remove unused import

--- cmd/ethermintd/root.go
+++ cmd/ethermintd/root.go
@@ -49,7 +49,6 @@ import (
 	authcmd "github.com/cosmos/cosmos-sdk/x/auth/client/cli"
 	"github.com/cosmos/cosmos-sdk/x/auth/types"
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
-	"github.com/cosmos/cosmos-sdk/x/crisis"
 	genutilcli "github.com/cosmos/cosmos-sdk/x/genutil/client/cli"
 	ethermintclient "github.com/evmos/ethermint/client"
 	"github.com/evmos/ethermint/crypto/hd"
