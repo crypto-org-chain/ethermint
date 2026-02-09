f332425ac4f272104434f128528527e64b2517e2
Randy Ang <randy.ang@crypto.com>
2026-02-09 16:16:58 +0800

    add pre-estimate bug comments

--- evmd/app.go
+++ evmd/app.go
@@ -800,6 +800,9 @@ func NewEthermintApp(
 		if workers == 0 {
 			workers = min(r.GOMAXPROCS(0), r.NumCPU())
 		}
+		// Setting as true if not block-stm wont work. Current bug in cosmos-sdk.
+		// Will revert to using the flag once the bug is fixed.
+		// https://github.com/cosmos/cosmos-sdk/issues/25879
 		preEstimate := true
 		coinDenom := func(ms storetypes.MultiStore) string {
 			denom := app.EvmKeeper.GetParams(sdk.NewContext(ms, cmtproto.Header{}, false, log.NewNopLogger())).EvmDenom
