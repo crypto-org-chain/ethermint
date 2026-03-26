3ed1ed6012aa0329c29cb62043496ac2888faefc
jay.tseng <jay.tseng@crypto.com>
2026-03-26 12:25:17 -0400

    clear unneed tests

--- x/evm/types/key_test.go
+++ x/evm/types/key_test.go
@@ -18,26 +18,6 @@ func TestAddressStoragePrefix(t *testing.T) {
 	require.Equal(t, addr.Bytes(), prefix[1:])
 }
 
-func TestAddressStoragePrefixDeterministic(t *testing.T) {
-	addr := common.HexToAddress("0xabcdef1234567890abcdef1234567890abcdef12")
-
-	// Multiple calls should return the same result
-	p1 := AddressStoragePrefix(addr)
-	p2 := AddressStoragePrefix(addr)
-	require.Equal(t, p1, p2)
-}
-
-func TestAddressStoragePrefixDifferentAddresses(t *testing.T) {
-	addr1 := common.HexToAddress("0x1111111111111111111111111111111111111111")
-	addr2 := common.HexToAddress("0x2222222222222222222222222222222222222222")
-
-	p1 := AddressStoragePrefix(addr1)
-	p2 := AddressStoragePrefix(addr2)
-	require.NotEqual(t, p1, p2)
-	// Both should share the same prefix byte
-	require.Equal(t, p1[0], p2[0])
-}
-
 func TestStateKey(t *testing.T) {
 	addr := common.HexToAddress("0x1234567890abcdef1234567890abcdef12345678")
 	key := common.HexToHash("0xdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeef")
