ddfb2275c66d138d464b2424c650681acd5eeb4b
songgaoye <217724508+songgaoye@users.noreply.github.com>
2026-07-20 14:32:45 +0800

    fix(rpc): exclude Cosmos gas from transaction receipts

--- rpc/backend/tx_info_test.go
+++ rpc/backend/tx_info_test.go
@@ -12,6 +12,7 @@ import (
 	"github.com/cometbft/cometbft/types"
 	dbm "github.com/cosmos/cosmos-db"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	ethtypes "github.com/ethereum/go-ethereum/core/types"
@@ -883,6 +884,61 @@ func (suite *BackendTestSuite) TestGetTransactionReceipt() {
 			}
 		})
 	}
+}
+
+func (suite *BackendTestSuite) TestGetTransactionReceipt_IgnoresPriorCosmosTxGas() {
+	bankTxBuilder := suite.backend.clientCtx.TxConfig.NewTxBuilder()
+	err := bankTxBuilder.SetMsgs(&banktypes.MsgSend{
+		FromAddress: suite.acc.String(),
+		ToAddress:   sdk.AccAddress(make([]byte, 20)).String(),
+		Amount:      sdk.NewCoins(sdk.NewInt64Coin("aphoton", 1)),
+	})
+	suite.Require().NoError(err)
+
+	bankTxBz, err := suite.backend.clientCtx.TxConfig.TxEncoder()(bankTxBuilder.GetTx())
+	suite.Require().NoError(err)
+	ethMsg, ethTxBz := suite.buildEthereumTxWithNonceAndGas(0, 100000)
+
+	blockResults := &tmrpctypes.ResultBlockResults{
+		Height: 1,
+		TxsResults: []*abci.ExecTxResult{
+			{Code: 0, GasUsed: 50000},
+			{
+				Code:    0,
+				GasUsed: 21000,
+				Events: []abci.Event{{
+					Type: evmtypes.EventTypeEthereumTx,
+					Attributes: []abci.EventAttribute{
+						{Key: evmtypes.AttributeKeyEthereumTxHash, Value: ethMsg.Hash().Hex()},
+						{Key: evmtypes.AttributeKeyTxIndex, Value: "0"},
+					},
+				}},
+			},
+		},
+	}
+
+	client := suite.backend.clientCtx.Client.(*mocks.Client)
+	queryClient := suite.backend.queryClient.QueryClient.(*mocks.EVMQueryClient)
+	var header metadata.MD
+	RegisterParamsWithoutHeader(queryClient, 1)
+	RegisterParams(queryClient, &header, 1)
+	_, err = RegisterBlockMultipleTxs(client, 1, []types.Tx{bankTxBz, ethTxBz})
+	suite.Require().NoError(err)
+	client.On("BlockResults", rpctypes.ContextWithHeight(1), mock.AnythingOfType("*int64")).Return(blockResults, nil)
+
+	suite.backend.indexer = indexer.NewKVIndexer(dbm.NewMemDB(), tmlog.NewNopLogger(), suite.backend.clientCtx)
+	block := types.MakeBlock(1, []types.Tx{bankTxBz, ethTxBz}, nil, nil)
+	suite.Require().NoError(suite.backend.indexer.IndexBlock(block, blockResults.TxsResults))
+
+	receiptByHash, err := suite.backend.GetTransactionReceipt(ethMsg.Hash(), nil)
+	suite.Require().NoError(err)
+	suite.Require().Equal(hexutil.Uint64(21000), receiptByHash["cumulativeGasUsed"])
+
+	blockNum := rpctypes.BlockNumber(1)
+	blockReceipts, err := suite.backend.GetBlockReceipts(rpctypes.BlockNumberOrHash{BlockNumber: &blockNum})
+	suite.Require().NoError(err)
+	suite.Require().Len(blockReceipts, 1)
+	suite.Require().Equal(blockReceipts[0]["cumulativeGasUsed"], receiptByHash["cumulativeGasUsed"])
 }
 
 // TestGetTransactionReceipt_BlockScopedWhenIndexerOverwritten verifies that when
